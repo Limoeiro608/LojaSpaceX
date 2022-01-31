8d7ee3ea4c100b7bcf22d6a927c5c604b2abf310
Gustavo Morais Trindade <946029@lojasrenner.com.br>
2022-01-31 18:40:18 -0300

    Desafio 3 com grid e melhor nomenclatura de classes

---
+++ src/components/Baseboard/Baseboard.jsx
@@ -0,0 +1,16 @@
+
+export const Baseboard = () => {
+
+    return (
+        <div className='Footer-content  text-center'>
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col'>
+                        <p>©2021 – Todos os direitos reservados</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,40 +4,42 @@ import { PaymentsMetods } from '../PaymentsMetods/PaymentsMetods';
 import Boleto from '../../assets/img/boleto.png'
 import Bitcoin from '../../assets/img/bitcoin.png'
 import MasterCard from '../../assets/img/master-card.png'
-
-
-
+import { Baseboard } from '../Baseboard/Baseboard';
 
 
 export const Footer = () => {
 
     return (
         <>
-            <div className="Footer">
-                <div className='Footer--flex'>
-                    <div>
-                        <img className='Footer__logo' src={ImgLogo} alt='logo' />
+            <div className='Footer'>
+                <div className="container">
+                    <div className='row text-center'>
+                        <div className='col-md-4'>
+                            <img className='Footer-logo' src={ImgLogo} alt='logo' />
+                        </div>
+                        <div className='col-md-8 text-md-start'>
+                            <h3 className='Footer-subtatile'> Institucional</h3>
+                            <Link href='#' nome='Termos de uso' />
+                            <Link href='#' nome='Politica de Privacidade' />
+                            <Link href='#' nome='Formas de Pagamento' />
+                            <Link href='#' nome='Trocas e Devoluções' />
+                        </div>
                     </div>
-                    <div>
-                        <h3 className='Footer__subtatile'> Institucional</h3>
-                        <Link href='#' nome='Termos de uso' />
-                        <Link href='#' nome='Politica de Privacidade' />
-                        <Link href='#' nome='Formas de Pagamento' />
-                        <Link href='#' nome='Trocas e Devoluções' />
+                    <div className='Footer-payments'>
+                        <div className='row'>
+                            <div className='col'>
+                                <h3 className='Footer-payments__title text-end'>Formas de Pagamento</h3>
+                                <ul className='Footer-payments__ul d-flex justify-content-end'>
+                                    <PaymentsMetods href='#' alt='boleto' src={Boleto} />
+                                    <PaymentsMetods href='#' alt='bitcoin' src={Bitcoin} />
+                                    <PaymentsMetods href='#' alt='master card' src={MasterCard} />
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className='Footer__payments-metods'>
-                    <h3 className='Footer__payments-metods__title'>Formas de Pagamento</h3>
-                    <ul className='Footer__payments-metods__ul'>
-                        <PaymentsMetods href='#' alt='boleto' src={Boleto}/>
-                        <PaymentsMetods href='#' alt='bitcoin' src={Bitcoin}/>
-                        <PaymentsMetods href='#' alt='master card' src={MasterCard}/>
-                    </ul>
-                </div>
-                <div className='Footer__content'>
-                    <p>©2021 – Todos os direitos reservados</p>
                 </div>
             </div>
+            <Baseboard/>
         </>
     );
 }
