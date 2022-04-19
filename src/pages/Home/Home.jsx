21e3aea1570daf5bdf79f6a7a1c00f21fcb5623b
gustavo_trindade25 <gustavo.trindade@compasso.com.br>
2022-04-19 15:51:13 -0300

    created component "newsBanner"

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,9 @@
 import {Banner} from '../../components/molecules/Banner/Banner'
 import {Footer} from '../../components/organisms/Footer/Footer'
-import MediaQuery from 'react-responsive'
 import {BannerSlide} from '../../components/molecules/BannerSlide/BannerSlide'
 import {BannerSlideMarks} from '../../components/molecules/BannerSlide/BannerSlideMarks'
-import {Video} from '../../components/atoms/Video/Video'
-import ImgThumb1 from '../../assets/img/thumb-mail-1.png'
-import ImgThumb2 from '../../assets/img/thumb-mail-2.png'
-import VideoRocket from '../../assets/video/rocket.mp4'
-import {SmallNewsText} from '../../components/molecules/SmallNewsText/SmallNewsText'
 import {HeaderDefault} from '../../layouts/HeaderDefault'
+import {NewsBanner} from '../../components/organisms/NewsBanner/NewsBanner'
 
 const Home = () => {
 	return (
@@ -25,25 +20,8 @@ const Home = () => {
 				<div className='Region col-12 mt-5'>
 					<BannerSlideMarks/>
 				</div>
-				<section className='Region col-12 container d-lg-flex mt-5 mb-5'>
-					<Video src={VideoRocket} width={'100%'} height={'100%'} poster={ImgThumb1} />
-					<SmallNewsText newsText='Pellentesque in ipsum id orci porta dapibus. Vestibulum ac diam sit .'
-						newsParagraphs='Pellentesque in ipsum id orci porta dapibus. Vestibulum ac diam sit .'
-					/>
-				</section>
-				<section className='Region col-12 container d-lg-flex mt-5 mb-5'>
-					<MediaQuery maxWidth={990}>
-						<Video src={VideoRocket} width={'100%'} height={'100%'} poster={ImgThumb2} />
-						<SmallNewsText newsText='Dragão Retorna à Terra'
-							newsParagraphs='Dragon completa a primeira missão civil em órbita'
-						/>
-					</MediaQuery>
-					<MediaQuery minWidth={991}>
-						<SmallNewsText newsText='Dragão Retorna à Terra'
-							newsParagraphs='Dragon completa a primeira missão civil em órbita'
-						/>
-						<Video src={VideoRocket} width={'100%'} height={'100%'} poster={ImgThumb2} />
-					</MediaQuery>
+				<section>
+					<NewsBanner/>
 				</section>
 				<div className='Region col-12 container'>
 					<Banner />
