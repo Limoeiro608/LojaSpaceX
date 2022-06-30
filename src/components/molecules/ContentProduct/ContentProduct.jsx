c6c68a8ea07b966c3bcc8533ab72e7287be2f878
gustavo_trindade25 <gustavo.trindade@compasso.com.br>
2022-06-30 01:21:27 -0300

    added spread operator

--- src/components/molecules/ContentProduct/ContentProduct.jsx
+++ src/components/molecules/ContentProduct/ContentProduct.jsx
@@ -5,11 +5,11 @@ export const ContentProduct = (props) => {
         <div className="ContentProduct">
             <div >
                 <h1 className="ContentProduct-title">{props.title}</h1>
-                <span className="ContentProduct-weight">{props.weight}</span>
+                <span className="ContentProduct-weight">{props.smallDescription}</span>
             </div>
             <div className="ContentProduct-prices">
-                <span className="ContentProduct-prices__last">R${props.lastPrice} </span>
-                <span className="ContentProduct-prices__current">&nbsp;R${props.currentPrice}</span>
+                <span className="ContentProduct-prices__last">R${props.oldPrice} </span>
+                <span className="ContentProduct-prices__current">&nbsp;R${props.price}</span>
             </div>
             <div>
                 <span className="ContentProduct-howManyTimes">{props.howManyTimes}</span>
